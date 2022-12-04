833a7803baa979a8f71ab17881e9f28ac948ba36
DLaaren <100743455+DLaaren@users.noreply.github.com>
2022-12-04 17:40:06 +0700

    Add files via upload

---
+++ lab2/build/CMakeFiles/tamagochi.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dlaaren/Desktop/lab2/gamebase/game.cpp" "CMakeFiles/tamagochi.dir/gamebase/game.cpp.o" "gcc" "CMakeFiles/tamagochi.dir/gamebase/game.cpp.o.d"
+  "/home/dlaaren/Desktop/lab2/gamebase/gameObject.cpp" "CMakeFiles/tamagochi.dir/gamebase/gameObject.cpp.o" "gcc" "CMakeFiles/tamagochi.dir/gamebase/gameObject.cpp.o.d"
+  "/home/dlaaren/Desktop/lab2/gamebase/textureManager.cpp" "CMakeFiles/tamagochi.dir/gamebase/textureManager.cpp.o" "gcc" "CMakeFiles/tamagochi.dir/gamebase/textureManager.cpp.o.d"
+  "/home/dlaaren/Desktop/lab2/main.cpp" "CMakeFiles/tamagochi.dir/main.cpp.o" "gcc" "CMakeFiles/tamagochi.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab2/build/CMakeFiles/tamagochi.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for tamagochi.
+# This may be replaced when dependencies are built.
